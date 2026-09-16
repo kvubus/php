0518adb092e51d372900e4a71e7dd36d01737f22
kuvbus <70339439+kvubus@users.noreply.github.com>
2026-09-16 11:32:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
     $x = 2;
     $y = 6;
 
-    echo (2*$x-0.5*$y)/4 ."<br>";
+    echo (2*$x-0.5*$y)/4."<br>";
 
     $x = 57;
     $y = 208;
-    echo "Iloczyn: " . $x & $y ."<br>";
-    echo "Suma: " . $x+$y ."<br>";
-    echo "Roznice bitowa: " . $x^$y;
+    echo "Iloczyn: ".($x & $y)."<br>";
+    echo "Suma: ".($x + $y)."<br>";
+    echo "Roznice bitowa: ".($x ^ $y);
     ?>
 </body>
 </html>
