2bf44e8e74dabdeb552cd817eab1a81e994b7bc6
kuvbus <70339439+kvubus@users.noreply.github.com>
2026-09-16 11:13:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello world <br>";
+    $a = 23;
+    $b = 7;
+
+    echo $a % $b."<br>";
+
+    $x = 2;
+    $y = 6;
+
+    echo (2*$x-0.5*$y) / 4."<br>";
+
+    $x = 57;
+    $y = 208;
+    echo "Iloczyn: ".$x*$y."<br>";
+    echo "Suma: ".$x+$y."<br>";
+    echo "Iloczyn: ".$x^$y
+    ?>
+</body>
+</html>
+
